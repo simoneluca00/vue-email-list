3fa84dcb92d342e77095b16b4acff18c81429d1f
simoneluca00 <si.zaino@libero.it>
2022-04-11 09:52:01 +0200

    Exercise finished

--- asset/js/main.js
+++ asset/js/main.js
@@ -4,22 +4,52 @@ var app = new Vue(
 
     data: {
         mails : [],
+        singleEmail : '',
     },
 
+    // mounted() o created()
     mounted(){
 
+        // ANCHOR versione arrow function (this) --> ciclo FOR
         for (let i = 0; i < 10; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then((res) => {
+                .then((res) => {
 
-                let singleEmail = res.data.response;
-                
-                if (!this.mails.includes(singleEmail)) {
-                    this.mails.push(singleEmail)
-                }
+                    let singleEmail = res.data.response;
+                    if (!this.mails.includes(singleEmail)) {
+                        this.mails.push(singleEmail)
+                    }
 
-            })     
+                })     
+
+        // ANCHOR versione con this salvato in una variabile --> ciclo FOR
+            // const self = this;
+            // axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            // .then(function(res){
+            //     self.singleEmail = res.data.response;
+
+            //     if (!self.mails.includes(self.singleEmail)) {
+            //         self.mails.push(self.singleEmail);
+            //     }
+            // })
         }
+
+        // ANCHOR versione con il ciclo WHILE
+        // let i = 0;
+        // while (i < 10) {
+        //     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        //     .then((res) => {
+
+        //         let singleEmail = res.data.response;
+                
+        //         if (!this.mails.includes(singleEmail)) {
+        //             this.mails.push(singleEmail)
+        //         }
+
+        //     })     
+
+        //     i++
+        // }
 
     },
 
